1001e576b380d2981b6f00616a808fdfe0b56230
Josh <joshua.boycehyland15@gmail.com>
2025-04-05 18:47:23 +0100

    #3.3 DAU calculation and visualisation

--- part3/functions.py
+++ part3/functions.py
@@ -3,3 +3,13 @@ import pandas as pd
 def get_unique_players( ):
     file = pd.read_csv("data/player_logged_in.csv") 
     return file['pid'].nunique()
+
+
+def get_monthtly_active_users():
+    file = pd.read_csv("data/player_logged_in.csv")
+    file['Time'] =  pd.to_datetime(file['Time']) # make surr in time format
+    file['Date'] =  file['Time'].dt.date # get date
+    daily_players = file.groupby('Date')['pid'].nunique().reset_index()
+    daily_players.columns = ['Date', 'UniqueActivePlayers']
+
+    return daily_players
