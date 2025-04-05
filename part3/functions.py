1dd5b8315b98298e45fa93439499161b5aca28fe
Josh <joshua.boycehyland15@gmail.com>
2025-04-05 19:54:18 +0100

    #3.5 Stickiness calculation and reflection

--- part3/functions.py
+++ part3/functions.py
@@ -22,3 +22,19 @@ def get_monthly_active_users():
     monthly_active_players.columns = ['Month', 'UniqueActivePlayers']
 
     return monthly_active_players
+
+def get_stickyness():
+    daly_active = get_daily_active_users()
+    monthly_active = get_monthly_active_users()
+
+    daly_active['YearMonth'] = pd.to_datetime(daly_active['Date']).dt.to_period('M').astype(str)
+
+    avg_daly_active = daly_active.groupby('YearMonth')['UniqueActivePlayers'].mean().reset_index()
+    avg_daly_active.columns = ['Month', 'AvgDAU']
+
+    monthly_active.columns = ['Month', 'MAU']
+
+    stickiness = pd.merge(avg_daly_active, monthly_active, on='Month')
+    stickiness['Stickiness'] = stickiness['AvgDAU'] / stickiness['MAU']
+
+    return stickiness
