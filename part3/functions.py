927dc6097c1f7fda68467008551dda2c2c053037
Josh <joshua.boycehyland15@gmail.com>
2025-04-05 20:32:44 +0100

    #3.6 Session analysis and visualisation

--- part3/functions.py
+++ part3/functions.py
@@ -38,3 +38,42 @@ def get_stickyness():
     stickiness['Stickiness'] = stickiness['AvgDAU'] / stickiness['MAU']
 
     return stickiness
+
+
+def get_totol_session_times():
+    file = pd.read_csv("data/player_logged_in.csv")
+    return len(file)
+
+def get_session_times():
+    file = pd.read_csv("data/player_logged_in.csv")
+    file['Time'] = pd.to_datetime(file(file['Time']))
+
+    # sort player and time
+    file = file.sort_values(['pid', 'Time'])
+
+    file['NextTime'] = file.groupby('pid')['Time'].shift(-1) # time diference between session and player
+    file['SessionLength'] = (file['NextTime'] - file['Time']).dt.total_seconds() / 60  # minutes
+
+    file = file[file['SessionLength'] > 0]
+
+    return file
+
+
+def get_median_session_times():
+    session_times = get_totol_session_times()
+    return session_times['SessionLength'].median()
+
+def get_avg_sessions_per_month():
+    
+    file= pd.read_csv("data/player_logged_in.csv")
+    file['Time'] = pd.to_datetime(file['Time'])
+    file['YearMonth'] = file['Time'].dt.to_period('M').astype(str)
+
+    
+    session_count = file.groupby('YearMonth').size().reset_index(name='TotalSessions')
+    unique_users = file.groupby('YearMonth')['pid'].nunique().reset_index(name='UniqueUsers')
+
+    median_session_times = pd.merge(session_count, unique_users, on='YearMonth')
+    median_session_times['AvgSessionsPerUser'] =median_session_times['TotalSessions'] / median_session_times['UniqueUsers']
+
+    return median_session_times
